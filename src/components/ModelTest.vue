0553c2713e6a9726fa2f2532b33cb41a0cc6ba72
mark <mark@protopie.io>
2019-04-24 17:25:19 +0900

    Model 데코레이터 설명 용

---
+++ src/components/ModelTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Model Test</h1>
+    <p>{{ checked ? "ON" : "OFF" }}</p>
+    <p>
+      <button v-on:click="changed">toggle</button>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Model, Prop } from "vue-property-decorator";
+
+@Component
+export default class ModelTest extends Vue {
+  @Prop()
+  @Model("change")
+  private checked!: boolean;
+
+  changed(event: Event) {
+    this.$emit("change", !this.checked);
+  }
+}
+</script>
+
